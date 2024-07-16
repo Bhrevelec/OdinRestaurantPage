f38d9b4aeb899cd60eb5edd2ab12bbfcc5457bfd
Bhrevelec <166656993+Bhrevelec@users.noreply.github.com>
2024-07-16 21:21:25 +0200

    added rudimentary page loading logic

--- src/index.js
+++ src/index.js
@@ -1,1 +1,43 @@
 import "./style.css";
+import { homepage } from "./home.js";
+import { menupage } from "./menu.js";
+import { aboutpage } from "./about.js";
+
+const content = document.querySelector("#content");
+const openPage = (tab) => {
+  //clear current content
+  content.innerHTML = "";
+
+  //load new content
+  switch (tab) {
+    case "home":
+      homepage();
+      break;
+    case "menu":
+      menupage();
+      break;
+    case "about":
+      aboutpage();
+      break;
+  }
+};
+
+openPage("home");
+
+const homeButton = document.querySelector("#homeBtn");
+const menuButton = document.querySelector("#menuBtn");
+const aboutButton = document.querySelector("#aboutBtn");
+
+homeButton.addEventListener("click", () => {
+  openPage("home");
+});
+
+menuButton.addEventListener("click", () => {
+  openPage("menu");
+});
+
+aboutButton.addEventListener("click", () => {
+  openPage("about");
+});
+
+//Init
