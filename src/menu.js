1edc2026544f93baf2a52e2c99ef68a7edfa0d7e
Bhrevelec <166656993+Bhrevelec@users.noreply.github.com>
2024-07-21 08:34:30 +0200

    added temporary menu page

--- src/menu.js
+++ src/menu.js
@@ -1,3 +1,36 @@
-const menupage = () => {};
+const menupage = () => {
+  const menupageDivContent = document.querySelector("#content");
+  menupageDivContent.classList.add("menupage");
+
+  const menupageHeader = document.createElement("header");
+  menupageHeader.setAttribute("class", "menupage");
+  menupageDivContent.appendChild(menupageHeader);
+
+  const menupageNav = document.createElement("nav");
+  menupageNav.setAttribute("class", "menupage");
+  menupageHeader.appendChild(menupageNav);
+
+  const menupageButtonHome = document.createElement("button");
+  menupageButtonHome.setAttribute("class", "menupage");
+  menupageButtonHome.setAttribute("id", "homeBtn");
+  menupageButtonHome.textContent = "Home";
+  menupageNav.appendChild(menupageButtonHome);
+
+  const menupageButtonMenu = document.createElement("button");
+  menupageButtonMenu.setAttribute("class", "menupage");
+  menupageButtonMenu.setAttribute("id", "menuBtn");
+  menupageButtonMenu.textContent = "Menu";
+  menupageNav.appendChild(menupageButtonMenu);
+
+  const menupageButtonAbout = document.createElement("button");
+  menupageButtonAbout.setAttribute("class", "menupage");
+  menupageButtonAbout.setAttribute("id", "aboutBtn");
+  menupageButtonAbout.textContent = "About";
+  menupageNav.appendChild(menupageButtonAbout);
+
+  const tbd = document.createElement("div");
+  tbd.textContent = "delicious food, just trust me";
+  menupageDivContent.appendChild(tbd);
+};
 
 export { menupage };
